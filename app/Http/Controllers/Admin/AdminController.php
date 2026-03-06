a488a039afb30f0927ee81c507f89ee47a4eebcf
isaac_touza@outlook.fr <isaac_touza@outlook.fr>
2026-03-06 18:33:36 +0100

    fix:telechargement de stand en pdf

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -264,7 +264,8 @@ class AdminController extends Controller
 public function generatePDF()
 {
     $stands = Stand::all();
-    $pdf = \Barryvdh\DomPDF\PDF::loadView('admin.stands_pdf', compact('stands'));
+    $pdf = Pdf::loadView('admin.stands_pdf', compact('stands'));
+   // $pdf = \Barryvdh\DomPDF\PDF::loadView('admin.stands_pdf', compact('stands'));
     return $pdf->download('liste_des_stands.pdf');
 }
 
